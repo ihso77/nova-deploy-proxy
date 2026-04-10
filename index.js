3a913e85289d1f9d9fad7eb62cb0404e18222f9f
Z User <z@container>
2026-04-10 11:17:03 +0000

    Add CORS headers

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
+
+// CORS - allow all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json({ limit: '5mb' }));
 
 const RAILWAY_API = 'https://backboard.railway.app/graphql/v2';
